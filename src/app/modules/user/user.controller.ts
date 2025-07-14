24dc1681b208439eb04fde1e328b8dab193415a8
khairul1036 <khairul4102.bd@gmail.com>
2025-07-14 18:04:24 +0600

    remove console line

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -6,8 +6,6 @@ const createUser = async (req: Request, res: Response) => {
   try {
     const { name, email } = req.body;
 
-    console.log({ name, email });
-
     const user = await User.create({
       name,
       email,
@@ -21,7 +19,6 @@ const createUser = async (req: Request, res: Response) => {
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
   } catch (error: any) {
     // eslint-disable-next-line no-console
-    console.log("errorrrrrrrrrrrrrrrr")
     console.log(error);
     res.status(httpCodes.BAD_REQUEST).json({
       message: `Something went wrong!! ${error.message}`,
